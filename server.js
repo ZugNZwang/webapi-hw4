3a3d788a8d0e343dce014ae2c29c3c121f8d31cb
ZugNZwang <eric.holguinsanchez@gmail.com>
2018-03-31 20:58:48 -0600

    GA Updates

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var Review = require('./Reviews');
 var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
 
+const crypto = require("crypto");
+var rp = require('request-promise');
+
 db = mongoose.createConnection(process.env.DB);
 
 var app = express();
@@ -18,9 +21,54 @@ app.use(passport.initialize());
 
 app.get('/', function(req, res) {
     res.json({ message: 'Assignment 4'});
-    });
+});
+
+const GA_TRACKING_ID = process.env.GA_KEY;
+
+function trackDimension(category, action, label, value, dimension, metric) {
+
+    var options = { method: 'GET',
+        url: 'https://www.google-analytics.com/collect',
+        qs:
+            {   // API Version.
+                v: '1',
+                // Tracking ID / Property ID.
+                tid: GA_TRACKING_ID,
+                // Random Client Identifier. Ideally, this should be a UUID that
+                // is associated with particular user, device, or browser instance.
+                cid: crypto.randomBytes(16).toString("hex"),
+                // Event hit type.
+                t: 'event',
+                // Event category.
+                ec: category,
+                // Event action.
+                ea: action,
+                // Event label.
+                el: label,
+                // Event value.
+                ev: value,
+                // Custom Dimension
+                cd1: dimension,
+                // Custom Metric
+                cm1: metric
+            },
+        headers:
+            {  'Cache-Control': 'no-cache' } };
+
+    return rp(options);
+}
 
 var router = express.Router();
+
+router.route('/test')
+    .get(function (req, res) {
+        // Event value must be numeric.
+        trackDimension('Feedback', 'Rating', 'Feedback for Movie', '1', 'Star Wars', '4')
+            .then(function (response) {
+                console.log(response.body);
+                res.status(200).send('Event tracked.').end();
+            })
+    });
 
 router.route('/postjwt')
     .post(authJwtController.isAuthenticated, function (req, res) {
